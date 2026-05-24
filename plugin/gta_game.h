8d50a4b4ffced0f491b227d341ebc3d4f0be3831
CodebyGPT <249225152+CodebyGPT@users.noreply.github.com>
2026-05-24 23:25:08 +0800

    Delete plugin directory

--- plugin/gta_game.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "batch_matching.h"
-
-namespace gta_game
-{
-    void register_patches(batch_matching& batch_matcher);
-}
--- plugin/gta_phone.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "batch_matching.h"
-
-namespace gta_phone
-{
-    void register_patches(batch_matching& batch_matcher);
-}
